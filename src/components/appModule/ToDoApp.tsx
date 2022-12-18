243f16239457452288470b399ff503cfbefb2f8c
SoSaymon <szymon.chirowski@protonmail.com>
2022-12-18 22:58:49 +0100

    Added cookie support to todo app module

--- src/components/appModule/ToDoApp.tsx
+++ src/components/appModule/ToDoApp.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {ToDoForm} from "./ToDoForm";
 import {TaskList} from "./TaskList";
+import Cookies from "js-cookie";
 
 interface Todo {
 	text: string;
@@ -11,6 +12,15 @@ export const TodoApp = () => {
 	// TODO: Add additional info to the todo item
 	const [todos, setTodos] = useState<Todo[]>([]);
 	const [input, setInput] = useState('');
+	const [removeAllCounter, setRemoveAllCounter] = useState(0);
+
+	useEffect(() => {
+		const todos = Cookies.get('todos');
+		console.log(todos);
+		if (todos) {
+			setTodos(JSON.parse(todos));
+		}
+	}, []);
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setInput(event.target.value);
@@ -26,6 +36,7 @@ export const TodoApp = () => {
 			if (formInput.style.borderColor === 'red') {
 				formInput.style.borderColor = '#2A3855';
 			}
+			Cookies.set('todos', JSON.stringify(todos), {expires: 365, path: '', sameSite: 'strict'});
 		} else {
 			formInput.style.borderColor = 'red';
 			alert('Please enter a todo item');
@@ -35,12 +46,20 @@ export const TodoApp = () => {
 
 	const handleRemoveAll = (event: React.FormEvent) => {
 		const formInput = document.querySelector('.todo__input') as HTMLInputElement;
-
 		event.preventDefault();
+		if (todos.length === 0) return;
+
+		if (removeAllCounter === 0) {
+			setRemoveAllCounter(1);
+			alert('Press again to confirm');
+			return;
+		}
 
 		formInput.style.borderColor = '#2A3855';
 		setInput('');
 		setTodos([]);
+		Cookies.remove('todos');
+		setRemoveAllCounter(0);
 	};
 
 	const handleRemove = (index: number) => {
